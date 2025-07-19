3488330f3620dfb8798a89ae03535606e56c48a7
Ciel <createdbycrt@gmail.com>
2025-07-19 17:54:43 +0800

    Setup aal

---
+++ include/cielmalloc/aal.hpp
@@ -0,0 +1,12 @@
+#ifndef CIELMALLOC_AAL_HPP_
+#define CIELMALLOC_AAL_HPP_
+
+#if defined(__aarch64__) || defined(_M_ARM64) || defined(__arm__) || defined(_M_ARM)
+#  include <cielmalloc/aal/arm.hpp>
+#elif defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
+#  include <cielmalloc/aal/x86.hpp>
+#else
+#  error "Unsupported architecture"
+#endif
+
+#endif // CIELMALLOC_AAL_HPP_
